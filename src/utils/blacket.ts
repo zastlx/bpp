56679a09b27dd03c3920843d6c9062ca3feeda3d
zastix <zastix@zastix.club>
2023-09-20 16:03:34 -0500

    we

--- src/utils/blacket.ts
+++ src/utils/blacket.ts
@@ -1,8 +1,7 @@
-import blacket from "@api/blacket";
 import { BlacketLocalUser, WithRequired } from "./types";
 
 function hasPlus(user: WithRequired<Partial<BlacketLocalUser>, "perms">) {
-    return (blacket().user.perms.includes("change_banner") && blacket().user.perms.includes("change_color") && blacket().user.perms.includes("early_access")) || blacket().user.role === "Plus";
+    return (user.perms.includes("change_banner") && user.perms.includes("change_color") && user.perms.includes("early_access")) || user.role === "Plus";
 }
 
 export { hasPlus };
